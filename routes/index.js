effe910ec97dfb6cca10a60d24d7d01b0ee816a6
Vladimir Stavitskiy <stavytskyivolodymyr@gmail.com>
2017-05-31 19:28:29 +0000

    add forms styling, refactor navbar...

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get("/", function(req, res) {
 
 // show register form
 router.get("/register", function(req, res) {
-   res.render("register"); 
+   res.render("register", {page: "register"}); 
 });
 
 //sign up logic
@@ -36,7 +36,7 @@ router.post("/register", function(req, res) {
 
 //show login form
 router.get("/login", function(req, res) {
-   res.render("login");
+   res.render("login", {page: "register"});
 });
 
 //login route
